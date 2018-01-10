2f0ce4922bc84fba63b73486f70f41b70051fd3e
Tuan Tran <mt12@williams.edu>
2018-01-10 18:49:38 -0500

    done single core conditional

--- src/object_space.rs
+++ src/object_space.rs
@@ -2,11 +2,27 @@ use std::collections::HashMap;
 use std::any::{Any, TypeId};
 
 pub trait ObjectSpace {
-    fn write<T: Default + Any + 'static>(&mut self, obj: T);
-    fn read<T: Default + Any + 'static>(&self) -> Option<&T>;
-    fn read_conditional<T: Default + Any + 'static>(&self, cond: fn(T) -> bool) -> Option<&T>;
-    fn take<T: Default + Any + 'static>(&mut self) -> Option<T>;
-    fn take_conditional<T: Default + Any + 'static>(&self, cond: fn(T) -> bool) -> Option<T>;
+    fn write<T>(&mut self, obj: T)
+    where
+        T: Default + Any + 'static;
+
+    fn read<T>(&self) -> Option<&T>
+    where
+        T: Default + Any + 'static;
+
+    fn read_conditional<P, T>(&self, cond: P) -> Option<&T>
+    where
+        P: FnMut(&T) -> bool,
+        T: Default + Any + 'static;
+
+    fn take<T>(&mut self) -> Option<T>
+    where
+        T: Default + Any + 'static;
+
+    fn take_conditional<P, T>(&mut self, cond: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+        T: Default + Any + 'static;
 }
 
 pub struct SequentialObjectSpace {
@@ -50,8 +66,21 @@ impl ObjectSpace for SequentialObjectSpace {
         }
     }
 
-    fn read_conditional<T: Default + Any + 'static>(&self, cond: fn(T) -> bool) -> Option<&T> {
-        None
+    fn read_conditional<P, T>(&self, cond: P) -> Option<&T>
+    where
+        P: FnMut(&T) -> bool,
+        T: Default + Any + 'static,
+    {
+        let type_id = TypeId::of::<T>();
+
+        if let Some(entry) = self.typeid_entries_dict.get(&type_id) {
+            match entry.as_any_ref().downcast_ref::<ObjectSpaceEntry<T>>() {
+                Some(ent) => ent.get_conditional(cond),
+                None => None,
+            }
+        } else {
+            None
+        }
     }
 
     fn take<T: Default + Any + 'static>(&mut self) -> Option<T> {
@@ -67,8 +96,21 @@ impl ObjectSpace for SequentialObjectSpace {
         }
     }
 
-    fn take_conditional<T: Default + Any + 'static>(&self, cond: fn(T) -> bool) -> Option<T> {
-        None
+    fn take_conditional<P, T>(&mut self, cond: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+        T: Default + Any + 'static,
+    {
+        let type_id = TypeId::of::<T>();
+
+        if let Some(entry) = self.typeid_entries_dict.get_mut(&type_id) {
+            match entry.as_any_mut().downcast_mut::<ObjectSpaceEntry<T>>() {
+                Some(ent) => ent.remove_conditional(cond),
+                None => None,
+            }
+        } else {
+            None
+        }
     }
 }
 
@@ -106,7 +148,27 @@ where
         self.object_list.first()
     }
 
+    fn get_conditional<P>(&self, cond: P) -> Option<&T>
+    where
+        P: FnMut(&T) -> bool,
+    {
+        match self.object_list.iter().position(cond) {
+            Some(index) => self.object_list.get(index),
+            None => None,
+        }
+    }
+
     fn remove(&mut self) -> Option<T> {
         self.object_list.pop()
+    }
+
+    fn remove_conditional<P>(&mut self, cond: P) -> Option<T>
+    where
+        P: FnMut(&T) -> bool,
+    {
+        match self.object_list.iter().position(cond) {
+            Some(index) => Some(self.object_list.remove(index)),
+            None => None,
+        }
     }
 }
