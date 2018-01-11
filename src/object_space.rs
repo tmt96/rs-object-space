95165ccdbb687ec8c48d6298d283b5db9770fa0f
Tuan Tran <mt12@williams.edu>
2018-01-11 00:32:52 -0500

    read and take all?

--- src/object_space.rs
+++ src/object_space.rs
@@ -1,28 +1,39 @@
 use std::collections::HashMap;
+use std::slice::Iter;
 use std::any::{Any, TypeId};
+use std::iter;
+use std::vec::Drain;
 
 pub trait ObjectSpace {
     fn write<T>(&mut self, obj: T)
     where
-        T: Default + Any + 'static;
+        T: Default + Any;
 
     fn read<T>(&self) -> Option<&T>
     where
-        T: Default + Any + 'static;
+        T: Default + Any;
 
     fn read_conditional<P, T>(&self, cond: P) -> Option<&T>
     where
         P: FnMut(&T) -> bool,
-        T: Default + Any + 'static;
+        T: Default + Any;
+
+    fn read_all<'a, T>(&'a self) -> Box<Iterator<Item = &T> + 'a>
+    where
+        T: Default + Any;
 
     fn take<T>(&mut self) -> Option<T>
     where
-        T: Default + Any + 'static;
+        T: Default + Any;
 
     fn take_conditional<P, T>(&mut self, cond: P) -> Option<T>
     where
         P: FnMut(&T) -> bool,
-        T: Default + Any + 'static;
+        T: Default + Any;
+
+    fn take_all<'a, T>(&'a mut self) -> Box<Iterator<Item = T> + 'a>
+    where
+        T: Default + Any;
 }
 
 pub struct SequentialObjectSpace {
@@ -38,7 +49,7 @@ impl SequentialObjectSpace {
 }
 
 impl ObjectSpace for SequentialObjectSpace {
-    fn write<T: Default + Any + 'static>(&mut self, obj: T) {
+    fn write<T: Default + Any>(&mut self, obj: T) {
         let default_entry = ObjectSpaceEntry::<T> {
             object_list: Vec::new(),
         };
@@ -53,7 +64,7 @@ impl ObjectSpace for SequentialObjectSpace {
         }
     }
 
-    fn read<T: Default + Any + 'static>(&self) -> Option<&T> {
+    fn read<T: Default + Any>(&self) -> Option<&T> {
         let type_id = TypeId::of::<T>();
 
         if let Some(entry) = self.typeid_entries_dict.get(&type_id) {
@@ -69,47 +80,76 @@ impl ObjectSpace for SequentialObjectSpace {
     fn read_conditional<P, T>(&self, cond: P) -> Option<&T>
     where
         P: FnMut(&T) -> bool,
-        T: Default + Any + 'static,
+        T: Default + Any,
     {
         let type_id = TypeId::of::<T>();
 
-        if let Some(entry) = self.typeid_entries_dict.get(&type_id) {
-            match entry.as_any_ref().downcast_ref::<ObjectSpaceEntry<T>>() {
+        match self.typeid_entries_dict.get(&type_id) {
+            Some(entry) => match entry.as_any_ref().downcast_ref::<ObjectSpaceEntry<T>>() {
                 Some(ent) => ent.get_conditional(cond),
                 None => None,
-            }
-        } else {
-            None
+            },
+            _ => None,
         }
     }
 
-    fn take<T: Default + Any + 'static>(&mut self) -> Option<T> {
+    fn read_all<'a, T>(&'a self) -> Box<Iterator<Item = &T> + 'a>
+    where
+        T: Default + Any,
+    {
         let type_id = TypeId::of::<T>();
 
-        if let Some(entry) = self.typeid_entries_dict.get_mut(&type_id) {
-            match entry.as_any_mut().downcast_mut::<ObjectSpaceEntry<T>>() {
+        match self.typeid_entries_dict.get(&type_id) {
+            Some(entry) => match entry.as_any_ref().downcast_ref::<ObjectSpaceEntry<T>>() {
+                Some(ent) => Box::new(ent.get_all()),
+                None => Box::new(iter::empty::<&T>()),
+            },
+            None => Box::new(iter::empty::<&T>()),
+        }
+    }
+
+    fn take<T: Default + Any>(&mut self) -> Option<T> {
+        let type_id = TypeId::of::<T>();
+
+        match self.typeid_entries_dict.get_mut(&type_id) {
+            Some(entry) => match entry.as_any_mut().downcast_mut::<ObjectSpaceEntry<T>>() {
                 Some(ent) => ent.remove(),
                 None => None,
-            }
-        } else {
-            None
+            },
+            _ => None,
         }
     }
 
     fn take_conditional<P, T>(&mut self, cond: P) -> Option<T>
     where
         P: FnMut(&T) -> bool,
-        T: Default + Any + 'static,
+        T: Default + Any,
     {
         let type_id = TypeId::of::<T>();
 
-        if let Some(entry) = self.typeid_entries_dict.get_mut(&type_id) {
-            match entry.as_any_mut().downcast_mut::<ObjectSpaceEntry<T>>() {
+        match self.typeid_entries_dict.get_mut(&type_id) {
+            Some(entry) => match entry.as_any_mut().downcast_mut::<ObjectSpaceEntry<T>>() {
                 Some(ent) => ent.remove_conditional(cond),
                 None => None,
-            }
-        } else {
-            None
+            },
+            _ => None,
+        }
+    }
+
+    fn take_all<'a, T>(&'a mut self) -> Box<Iterator<Item = T> + 'a>
+    where
+        T: Default + Any,
+    {
+        let type_id = TypeId::of::<T>();
+
+        match self.typeid_entries_dict.get_mut(&type_id) {
+            Some(entry) => entry
+                .as_any_mut()
+                .downcast_mut::<ObjectSpaceEntry<T>>()
+                .map_or(Box::new(iter::empty::<T>()), |ent| {
+                    Box::new(ent.remove_all())
+                }),
+            None => Box::new(iter::empty::<T>()),
         }
     }
 }
@@ -119,13 +159,13 @@ trait ObjectSpaceEntryFamily {
     fn as_any_mut(&mut self) -> &mut Any;
 }
 
-struct ObjectSpaceEntry<T: Default + Any + 'static> {
+struct ObjectSpaceEntry<T: Default + Any> {
     object_list: Vec<T>,
 }
 
 impl<T> ObjectSpaceEntryFamily for ObjectSpaceEntry<T>
 where
-    T: Default + Any + 'static,
+    T: Default + Any,
 {
     fn as_any_ref(&self) -> &Any {
         self
@@ -138,7 +178,7 @@ where
 
 impl<T> ObjectSpaceEntry<T>
 where
-    T: Default + Any + 'static,
+    T: Default + Any,
 {
     fn add(&mut self, obj: T) {
         &self.object_list.push(obj);
@@ -158,6 +198,10 @@ where
         }
     }
 
+    fn get_all(&self) -> Iter<T> {
+        self.object_list.iter()
+    }
+
     fn remove(&mut self) -> Option<T> {
         self.object_list.pop()
     }
@@ -166,9 +210,13 @@ where
     where
         P: FnMut(&T) -> bool,
     {
-        match self.object_list.iter().position(cond) {
-            Some(index) => Some(self.object_list.remove(index)),
-            None => None,
-        }
+        self.object_list
+            .iter()
+            .position(cond)
+            .map(|index| self.object_list.remove(index))
+    }
+
+    fn remove_all(&mut self) -> Drain<T> {
+        self.object_list.drain(..)
     }
 }
