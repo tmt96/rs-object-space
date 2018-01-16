bf95fdc3423b0ecbf69026922295f6405daa5f26
Tuan Tran <mt12@williams.edu>
2018-01-16 12:25:10 -0500

    some editing

--- src/type_box.rs
+++ src/type_box.rs
@@ -2,26 +2,27 @@ use std::any::Any;
 use std::marker::PhantomData;
 
 pub trait TypeFamily {
-    fn is_type<U>(&self, t: &Type<U>) -> bool
+    fn is_type<U>(t: &Type<U>) -> bool
     where
         U: Default + Any;
-    fn is_subtype_of<U>(&self, t: &Type<U>) -> bool
+    fn is_subtype_of<U>(t: &Type<U>) -> bool
     where
         U: Default + Any;
-    fn is_supertype_of<U>(&self, t: &Type<U>) -> bool
+    fn is_supertype_of<U>(t: &Type<U>) -> bool
     where
         U: Default + Any;
 }
 
+#[derive(Hash, Eq)]
 pub struct Type<T: Default + Any> {
     marker: PhantomData<T>,
 }
 
-impl<T> TypeFamily for Type<T>
+impl<T> Type<T>
 where
     T: Default + Any,
 {
-    fn is_type<U>(&self, _: &Type<U>) -> bool
+    fn is_type_helper<U>() -> bool
     where
         U: Default + Any,
     {
@@ -29,18 +30,47 @@ where
         obj.is::<U>()
     }
 
-    fn is_subtype_of<U>(&self, _: &Type<U>) -> bool
+    fn is_subtype_of_helper<U>() -> bool
     where
         U: Default + Any,
     {
         let obj: &Any = &T::default();
         obj.downcast_ref::<U>().is_some()
     }
+}
 
-    fn is_supertype_of<U>(&self, t: &Type<U>) -> bool
+impl<T, U> PartialEq<Type<U>> for Type<T>
+where
+    T: Default + Any,
+    U: Default + Any,
+{
+    fn eq(&self, _: &Type<U>) -> bool {
+        Self::is_type_helper::<U>()
+    }
+}
+
+impl<T> TypeFamily for Type<T>
+where
+    T: Default + Any,
+{
+    fn is_type<U>(_: &Type<U>) -> bool
     where
         U: Default + Any,
     {
-        t.is_subtype_of::<T>(self)
+        Self::is_type_helper::<U>()
+    }
+
+    fn is_subtype_of<U>(_: &Type<U>) -> bool
+    where
+        U: Default + Any,
+    {
+        Self::is_subtype_of_helper::<U>()
+    }
+
+    fn is_supertype_of<U>(_: &Type<U>) -> bool
+    where
+        U: Default + Any,
+    {
+        Type::<U>::is_subtype_of_helper::<T>()
     }
 }
